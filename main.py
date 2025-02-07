327628d4a4b2903fa784c6d1669c47004d09d0e1
Tristan <tristan1.laubscher@gmail.com>
2025-02-07 15:05:10 +0200

    Some Updates
    Added function to find rgb value for pixel from the binary r g and b values and started to test tkinter as the library to build the output on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# FUNCTIONS
-def binary_to_int(binary: int) -> int:
+# Functions
+def binary_to_int(binary: str) -> int:
     binary = binary[::-1]
     final: int = 0
 
@@ -9,12 +9,20 @@ def binary_to_int(binary: int) -> int:
     return final
 
 
-def read_bitmap(bitmap: str) -> None:
-    pixels_outputted: int = 0
-    for i in range(0, len(bitmap), COLOUR_DEPTH):
-        pixel_data = bitmap[i:i+COLOUR_DEPTH]
+def find_rgb(binary: str) -> int:
+    binary_coefficient: int = int(255/(2**len(binary)-1))
+    return binary_to_int(binary)*binary_coefficient
 
-        print(COLOURS[pixel_data], end='')
+
+def read_bitmap(bitmap: str, _COLOUR_DEPTH) -> None:
+    pixels_outputted: int = 0
+    for i in range(0, len(bitmap), _COLOUR_DEPTH):
+        pixel_data = bitmap[i:i+_COLOUR_DEPTH]
+
+        print(  find_rgb(pixel_data[:int(_COLOUR_DEPTH/3)]),
+                find_rgb(pixel_data[int(_COLOUR_DEPTH/3):int(_COLOUR_DEPTH/3*2)]),
+                find_rgb(pixel_data[int(_COLOUR_DEPTH/3*2):int(_COLOUR_DEPTH)]),
+                end=' | ')
         pixels_outputted += 1
 
         # Check If End Of Row
@@ -22,21 +30,19 @@ def read_bitmap(bitmap: str) -> None:
             print()
 
 
+
 # ---------------------------------------------------------------------------------
 
-# FILE
 with open("bitmap_test.txt", 'r') as bitmap_file:
-    bitmap: str = bitmap_file.read()
+    bitmap: str = bitmap_file.read().replace(' ', '').replace('\n', '')
     bitmap_file.close()
 
-# META DATA (stored in the first set of bits)
 WIDTH: int = binary_to_int(bitmap[0:4])
 HEIGHT: int = binary_to_int(bitmap[4:8])
-COLOUR_DEPTH: int = 2
-COLOURS: dict = {'11':'@', '00':'*', '10':'~', '01':'^'}
-print(f"Height: {HEIGHT}\nWidth: {WIDTH}")
+COLOUR_DEPTH: int = binary_to_int(bitmap[8:12])
+print(f"Height: {HEIGHT} | Width: {WIDTH} | Colour Depth: {COLOUR_DEPTH}")
 
-# REMOVE META DATA
-bitmap = bitmap[8:] if bitmap.find('\n') == -1 else bitmap[9:]
+# Remove Meta Data
+bitmap = bitmap[12:]
 
-read_bitmap(bitmap)
+read_bitmap(bitmap, COLOUR_DEPTH)
---
+++ ui-test.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+def rgb_to_hex(rgb: tuple) -> str:
+    hex_value: str = ""
+    for value in rgb:
+        current_hex: str = hex(value).replace('0x','')
+        hex_value += (current_hex if len(current_hex) > 1 else f"0{current_hex}")
+    return hex_value
+
+root = Tk()
+root.geometry('300x500')
+root.title('Test')
+root.config(bg=f"#{rgb_to_hex((170, 255, 255))}")
+
+root.mainloop()
